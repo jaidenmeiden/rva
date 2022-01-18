5d0a033fc3bee5054ed1db2b62bb615715a16bcc
Jaiden Meiden <jaidenmeiden@gmail.com>
2022-01-18 08:07:12 +0100

    feat: Add health and damage scripts

---
+++ DragonAttackOculus/Assets/Scripts/Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public DamageType type = DamageType.enemy;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public float HealthPoints{
+
+        get{
+            return healthPoints;
+        }
+
+        set{
+            healthPoints = value;
+
+            if(healthPoints <= 0){
+                //TODO: gestionar la muerte del personaje / enemigo
+            }
+        }
+    }
+
+    [SerializeField]
+    private float healthPoints = 100.0f;
+}
