294a23e564904c9035ed59caea8e83a625e542b3
Jaiden Meiden <jaidenmeiden@gmail.com>
2022-01-22 21:14:30 +0100

    feat: Adjust follow destination script

--- DragonAttackOculus/Assets/Scripts/FollowDestination.cs
+++ DragonAttackOculus/Assets/Scripts/FollowDestination.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class FollowDestination : MonoBehaviour
 {
+    GameObject[] destinations;
+    private Transform destination;
+    public float speed = 5.0f;
+    private GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        destinations = GameObject.FindGameObjectsWithTag("Waypoints");
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        destination = destinations[Random.Range(0, destinations.Length)].transform;
     }
 
     // Update is called once per frame
