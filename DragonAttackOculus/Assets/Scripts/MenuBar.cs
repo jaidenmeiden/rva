f17ae953a677a56d41845a71c3c260a2ae2a1a0e
Jaiden Meiden <jaidenmeiden@gmail.com>
2022-01-22 11:19:03 +0100

    feat: Complete menu bar script algorithm

--- DragonAttackOculus/Assets/Scripts/MenuBar.cs
+++ DragonAttackOculus/Assets/Scripts/MenuBar.cs
@@ -3,19 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum BarType
+{
+    health,
+    mana
+}
 public class MenuBar : MonoBehaviour
 {
     Slider _slider;
+    public GameObject target;
+    public BarType barType;
     
     // Start is called before the first frame update
     void Start()
     {
         _slider = GetComponent<Slider>();
+
+        switch (barType)
+        {
+            case BarType.health:
+                _slider.maxValue = target.GetComponent<Health>().HealthPoints;
+                break;
+            case BarType.mana:
+                _slider.maxValue = Weapons.MAGIC_COOLDOWN_TIME;
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (barType)
+        {
+            case BarType.health:
+                _slider.value = target.GetComponent<Health>().HealthPoints;
+                break;
+            case BarType.mana:
+                _slider.value = target.GetComponent<Weapons>().magicCooldown;
+                break;
+        }
     }
 }
