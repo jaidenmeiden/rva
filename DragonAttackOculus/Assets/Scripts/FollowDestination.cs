a32c636c50b7094beec889ec50fbfab135f73e4b
Jaiden Meiden <jaidenmeiden@gmail.com>
2022-01-22 21:32:13 +0100

    feat: Adjust translation and rotation

--- DragonAttackOculus/Assets/Scripts/FollowDestination.cs
+++ DragonAttackOculus/Assets/Scripts/FollowDestination.cs
@@ -12,7 +12,7 @@ public class FollowDestination : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        destinations = GameObject.FindGameObjectsWithTag("Waypoints");
+        destinations = GameObject.FindGameObjectsWithTag("Waypoint");
         player = GameObject.FindGameObjectWithTag("Player");
 
         destination = destinations[Random.Range(0, destinations.Length)].transform;
@@ -21,6 +21,16 @@ public class FollowDestination : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Translation
+        float space = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, destination.position, space);
+
+        // Rotation
         
+        Vector3 targetDirection = destination.position - transform.position;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, space, 0);
+        
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+        transform.rotation = Quaternion.LookRotation(newDirection);
     }
 }
