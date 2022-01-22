6736a0fe096fc81570ccc712aee3fcfd414971e3
Jaiden Meiden <jaidenmeiden@gmail.com>
2022-01-22 10:11:07 +0100

    feat: Adjust menu bar script

--- DragonAttackOculus/Assets/Scripts/MenuBar.cs
+++ DragonAttackOculus/Assets/Scripts/MenuBar.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuBar : MonoBehaviour
 {
+    Slider _slider;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _slider = GetComponent<Slider>();
     }
 
     // Update is called once per frame
